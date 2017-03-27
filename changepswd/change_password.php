8559a1ccfb9abda59dc50e35694cd67b76c5a163
akonna01 <akonna01@cs.ucy.ac.cy>
2017-03-27 12:49:41 +0100

    change password added

---
+++ changepswd/change_password.php
@@ -0,0 +1,101 @@
+<?php
+// $_SESSION["userId"] = "24";
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "cyberethics";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+
+if(count($_POST)>0) {
+$result = $conn->query("SELECT *from operator WHERE username='" . $_SESSION["user"] . "'");
+$row=$result->fetch_assoc();
+if($_POST["currentPassword"] == $row["password"]) {
+$conn->query("UPDATE operator set password='" . $_POST["newPassword"] . "' WHERE username='" . $_SESSION["user"] . "'");
+$message = "Password Changed";
+echo ("<SCRIPT LANGUAGE='JavaScript'>
+            window.alert('Password Changed')
+            window.location.href='/leit/desp.php';
+            </SCRIPT>");
+
+} else echo ("<SCRIPT LANGUAGE='JavaScript'>
+            window.alert('Current Password is not correct')
+     
+            </SCRIPT>");
+
+}
+?>
+<html>
+<head>
+<title>Change Password</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+<script>
+function validatePassword() {
+var currentPassword,newPassword,confirmPassword,output = true;
+
+currentPassword = document.frmChange.currentPassword;
+newPassword = document.frmChange.newPassword;
+confirmPassword = document.frmChange.confirmPassword;
+
+if(!currentPassword.value) {
+	currentPassword.focus();
+	document.getElementById("currentPassword").innerHTML = "required";
+	output = false;
+}
+else if(!newPassword.value) {
+	newPassword.focus();
+	document.getElementById("newPassword").innerHTML = "required";
+	output = false;
+}
+else if(!confirmPassword.value) {
+	confirmPassword.focus();
+	document.getElementById("confirmPassword").innerHTML = "required";
+	output = false;
+}
+if(newPassword.value != confirmPassword.value) {
+	newPassword.value="";
+	confirmPassword.value="";
+	newPassword.focus();
+	document.getElementById("confirmPassword").innerHTML = "not same";
+	output = false;
+} 	
+return output;
+}
+</script>
+</head>
+<body>
+<form name="frmChange" method="post" action="" onSubmit="return validatePassword()">
+<div style="width:500px;">
+<table border="0" cellpadding="10" cellspacing="0" width="500" align="center" class="tblSaveForm">
+<tr class="tableheader">
+<td colspan="2">Change Password</td>
+</tr>
+<tr>
+<td width="40%"><label>Current Password</label></td>
+<td width="60%"><input type="password" name="currentPassword" class="txtField"/><span id="currentPassword"  class="required"></span></td>
+</tr>
+<tr>
+<td><label>New Password</label></td>
+<td><input type="password" name="newPassword" class="txtField"/><span id="newPassword" class="required"></span></td>
+</tr>
+<td><label>Confirm Password</label></td>
+<td><input type="password" name="confirmPassword" class="txtField"/><span id="confirmPassword" class="required"></span></td>
+</tr>
+<tr>
+<td colspan="2"><input type="submit" name="submit" value="Submit" class="btnSubmit"></td>
+</tr>
+</table>
+</div>
+</form>
+</body></html>
